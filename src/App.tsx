255cb3576aa83f9223a6bb148bbb15e6e128a848
Dionys <117740720+denyskozak@users.noreply.github.com>
2026-04-22 19:04:39 +0100

    Increase global typography scale and readability

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChallengeWorkspace } from './components/challenge/challenge-workspace';
 
 export default function App() {
   return (
-    <Theme appearance="dark" accentColor="indigo" grayColor="slate" radius="medium" scaling="100%">
+    <Theme appearance="dark" accentColor="indigo" grayColor="slate" radius="medium" scaling="110%">
       <Container size="4" p="3">
         <ChallengeWorkspace />
       </Container>
